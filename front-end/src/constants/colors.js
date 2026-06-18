ec30912e7496f2ae79b14d75bcb04488aecd8e53
avytran <tr.anhvy.work@gmail.com>
2026-06-19 00:54:14 +0700

    feat: add constants and config

---
+++ front-end/src/constants/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  coffee50: "#FDF8F5", coffee100: "#F7EBE1", coffee200: "#EED5C1",
+  coffee300: "#E4BEA0", coffee400: "#DDB892", coffee500: "#B08968",
+  coffee600: "#9C6644", coffee700: "#7F5539", coffee800: "#5C3A21", coffee900: "#4A2E1B",
+  forest50: "#F2F7F5", forest100: "#E1EFE8", forest200: "#C4DFD2",
+  forest300: "#9BCCB7", forest400: "#6CB297", forest500: "#4A9D7D",
+  forest600: "#357F63", forest700: "#2A6550", forest800: "#245242", forest900: "#1B4332",
+};
